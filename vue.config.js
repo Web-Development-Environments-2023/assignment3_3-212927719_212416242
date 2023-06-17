43d55d5e0b041e761fb1cb320b0fa755dc74df4f
omer aflalo <omer.aflalo@gmail.com>
2023-06-17 17:37:08 +0300

    login working

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     devtool: "source-map"
   },
   devServer: {
-    // host: "132.72.65.211"
+    // host: "132.73.84.133"
     host: "localhost"
   }
   // publicPath: process.env.NODE_ENV === "production" ? "/LAB12/" : "/"
