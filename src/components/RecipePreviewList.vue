19ad16cd3e72383b7565ba264f7efecb13edd119
omer aflalo <omer.aflalo@gmail.com>
2023-06-21 23:15:05 +0300

    update RecipePreviewList

--- src/components/RecipePreviewList.vue
+++ src/components/RecipePreviewList.vue
@@ -6,7 +6,7 @@
     </h3>
     <b-row>
       <b-col v-for="r in recipes" :key="r.id">
-        <RecipePreview class="recipePreview" :recipe="r" />
+        <RecipePreview class="recipePreview" :recipe="r.recipe" :id="r.id" />
       </b-col>
     </b-row>
   </b-container>
@@ -14,42 +14,53 @@
 
 <script>
 import RecipePreview from "./RecipePreview.vue";
+import tempData from "../../tempRecipes.js";
 export default {
   name: "RecipePreviewList",
   components: {
-    RecipePreview
+    RecipePreview,
   },
   props: {
     title: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
+    path: {
+      type: String,
+      required: true,
+    },
   },
   data() {
     return {
-      recipes: []
+      recipes: [],
     };
   },
   mounted() {
     this.updateRecipes();
   },
+  watch: {
+    path: function() {
+      this.updateRecipes();
+    },
+  },
   methods: {
     async updateRecipes() {
       try {
-        const response = await this.axios.get(
-          this.$root.store.server_domain + "/users/lastWatchRecipes?limit=3",
-        );
+        // const response = await this.axios.get(
+        //   this.$root.store.server_domain + this.path
+        // );
 
         // console.log(response);
-        const recipes = response.data.recipes;
+        // const recipes = response.data.recipes;
+        const recipes = tempData.getrecipes();
         this.recipes = [];
         this.recipes.push(...recipes);
         // console.log(this.recipes);
       } catch (error) {
         console.log(error);
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
